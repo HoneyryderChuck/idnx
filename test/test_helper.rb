ca11c6acbe08dabe83ee6a85b8f692ab9b5717b9
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-12-15 09:39:53 +0000

    turning on warnings for latest in CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "idnx"
 
 require "minitest/autorun"
+
+if RUBY_VERSION >= "3.4.0"
+  Warning.categories.each do |cat|
+    Warning[cat] = true
+  end
+end
